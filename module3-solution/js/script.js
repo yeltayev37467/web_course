4e84fbfb4492f34f3f6d9653f3e57488caac5f04
yeltayev37467 <37467@iitu.edu.kz>
2024-12-01 01:45:07 +0500

    project commit

---
+++ module3-solution/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // Collapse the navbar after clicking on a link
+    $('.navbar-nav > li > a').click(function() {
+        $('#navbarCollapse').collapse('hide');
+    });
+});
